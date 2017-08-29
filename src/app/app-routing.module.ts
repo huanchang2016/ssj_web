623aa90fbddb263192b6297d6177f6aec100d4d0
zhanghuanchang <380019340@qq.com>
2017-08-29 09:08:22 +0800

    重新设计路由，备份

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent }   from './user/home/home.component';
-import { ZhaopinComponent }   from './user/zhaopin/zhaopin.component';
+import { IndexComponent } from './user/index/index.component';
+import { HomeComponent } from './user/home/home.component';
+import { ZhaopinComponent } from './user/zhaopin/zhaopin.component';
 import { JobsDetailComponent } from './user/zhaopin/jobs-detail/jobs-detail.component';
 import { CompanyComponent } from './user/zhaopin/company/company.component';
 import { CompanyHomeComponent } from './user/zhaopin/company/company-home/company-home.component';
 import { CompanyJobsComponent } from './user/zhaopin/company/company-jobs/company-jobs.component';
 import { SchoolRecruitComponent } from './user/zhaopin/company/school-recruit/school-recruit.component';
 import { CompanyAskComponent } from './user/zhaopin/company/company-ask/company-ask.component';
-import { RegisterComponent }    from './modules/register/register.component';
+import { RegisterComponent } from './modules/register/register.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: 'zhaopin', pathMatch: 'full'},
+
+  { path: '', redirectTo: 'zhaopin', pathMatch: 'full'},
   { path: 'home', component: HomeComponent, data: { title: '天府菁英网' }},
   { path: 'zhaopin', component: ZhaopinComponent, data: { title: '天府新区管委会招聘信息-天府菁英网' } },
   { path: "jobsDetails/:jobId", component: JobsDetailComponent, data: {title: "个人简历详情-天府新区管委会招聘信息"}},
@@ -25,8 +27,7 @@ const routes: Routes = [
           { path: 'ask', component: CompanyAskComponent, data: {title:"公司主页-公司详情-天府新区管委会招聘信息"}}
 
   ]},
-  { path: "register", component: RegisterComponent, data: { title: "用户注册-天府新区管委会"} },
-  // { path: "**", component: HomeComponent, data: { title: '天府菁英网-首页' } }
+  { path: "**", component: HomeComponent, data: { title: '天府菁英网-首页' } }
 
 ];
 
--- src/app/modules/header/header.component.ts
+++ src/app/modules/header/header.component.ts
@@ -12,14 +12,14 @@ export class HeaderComponent implements OnInit {
   //传递给导航组件的参数
   public navListContent = {
     menu:[
-      { title: "首页", link: "/home"},
-      { title: "招聘", link: '/zhaopin'},
+      { title: "首页", link: "./home"},
+      { title: "招聘", link: './zhaopin'},
       { title: "子菜单", childrenNav: [
-            {childTitle: "子栏目1", childLink: "/xxx"},
-            {childTitle: "子栏目2", childLink: "/xxx"},
+            {childTitle: "子栏目1", childLink: "./xxx"},
+            {childTitle: "子栏目2", childLink: "./xxx"},
           ]
       },
-      { title: "other Link", link: '/xxx'}
+      { title: "other Link", link: './xxx'}
     ],
     css:[
       { _selector:'nav-list', _attr: "background", _value: "#009cff"}
